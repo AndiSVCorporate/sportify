29ce6a0544c8760dfeefdbc46a004cbdb83f7233
AlexanderLenz85@gmail.com <AlexanderLenz85@gmail.com@d4d58bc8-d553-6bc2-2287-e566861c44a8>
2012-10-16 08:02:23 +0000

    initial commit

---
+++ trunk/SportifyAndroidApp/src/com/msports/sportify/android/Session.java
@@ -0,0 +1,83 @@
+package com.msports.sportify.android;
+
+public class Session {
+
+	private int stepsToday;
+	private int avgStepFrequency;
+	private int maxStepFrequency;
+	private long time;
+	private int temperature;
+	private int condition;
+	private int calories;
+	private int avgHeartRate;
+	private int maxHeartRate;
+	
+	public Session(int stepsToday,  long time, int temperature) {
+		this.stepsToday = stepsToday;
+		this.time = time;
+		this.temperature = temperature;
+	}
+	
+	public Session() {
+		this.stepsToday = 1;
+		this.time = 12;
+		this.temperature = 123;
+	}
+	
+	public int getStepsToday() {
+		return stepsToday;
+	}
+	public void setStepsToday(int stepsToday) {
+		this.stepsToday = stepsToday;
+	}
+	public int getAvgStepFrequency() {
+		return avgStepFrequency;
+	}
+	public void setAvgStepFrequency(int avgStepFrequency) {
+		this.avgStepFrequency = avgStepFrequency;
+	}
+	public int getMaxStepFrequency() {
+		return maxStepFrequency;
+	}
+	public void setMaxStepFrequency(int maxStepFrequency) {
+		this.maxStepFrequency = maxStepFrequency;
+	}
+	public long getTime() {
+		return time;
+	}
+	public void setTime(long time) {
+		this.time = time;
+	}
+	public int getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+	public int getCondition() {
+		return condition;
+	}
+	public void setCondition(int condition) {
+		this.condition = condition;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public int getAvgHeartRate() {
+		return avgHeartRate;
+	}
+	public void setAvgHeartRate(int avgHeartRate) {
+		this.avgHeartRate = avgHeartRate;
+	}
+	public int getMaxHeartRate() {
+		return maxHeartRate;
+	}
+	public void setMaxHeartRate(int maxHeartRate) {
+		this.maxHeartRate = maxHeartRate;
+	}
+	
+	
+}
---
+++ trunk/SportifyAndroidApp/src/com/msports/sportify/android/UploadFragment.java
@@ -0,0 +1,77 @@
+package com.msports.sportify.android;
+
+import com.google.gson.Gson;
+import com.msports.sportify.android.SportifyHttpClient.RequestMethod;
+
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UploadFragment extends Fragment implements OnClickListener{
+
+	private Button storeBtn;
+	private EditText stepsTodayEditText;
+	private View v;
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.upload, null);
+		storeBtn = (Button) view.findViewById(R.id.storeSessionButton);
+		stepsTodayEditText = (EditText) view.findViewById(R.id.editText1);
+		
+		storeBtn.setOnClickListener(this);
+		v = view;
+		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.storeSessionButton) {
+			stepsTodayEditText = (EditText)getActivity().findViewById(R.id.editText1);
+//			stepsTodayEditText = (EditText)v.findViewById(R.id.editText1);
+			String numberAsString = stepsTodayEditText.getText().toString();
+			
+			int steps = new Integer(numberAsString);
+			
+			sendStoringRequest(steps);
+			
+		}
+	}
+	
+	public static final String STORE_SESSION_URL = "http://sportify-msports.appspot.com/storeSession";
+	public static final String PARAM_DATA = "data";
+	
+	private void sendStoringRequest(int steps) {
+		RestClient client = new RestClient(STORE_SESSION_URL);
+		Session session = new Session(steps, System.currentTimeMillis(), 10);
+		Gson gson = new Gson();
+			String json = gson.toJson(session);
+			String body = json.toString();
+			Log.i("JSON-Request", body);
+			client.addParam(PARAM_DATA, body);
+		
+
+		// execute request
+		try {
+			client.execute(RequestMethod.GET);
+		} catch (Exception e) {
+			Log.e("Request-Exception:", e.toString());
+		}
+
+		int responseCode = client.getResponseCode();
+		if (responseCode == 200) {
+			// HTTP response is OK
+			String response = client.getResponse();
+			Log.i("Servlet-Response", response);
+			
+		}
+	}
+}
