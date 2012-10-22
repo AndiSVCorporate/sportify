297a1e30a5a6ea0f183d5ca2a83400d0ae791832
AlexanderLenz85 <AlexanderLenz85@gmail.com>
2012-10-22 20:16:56 +0000

    ... rpc

--- Sportify/src/com/msports/sportify/client/GreetingServiceAsync.java
+++ Sportify/src/com/msports/sportify/client/GreetingServiceAsync.java
@@ -2,7 +2,6 @@ package com.msports.sportify.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.msports.sportify.shared.DailyStepsData;
-import com.msports.sportify.shared.DailyStepsEntry;
 
 /**
  * The async counterpart of <code>GreetingService</code>.
